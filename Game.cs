115a23e26f77218b8810540b01e8d04893b2cde1
LukasGG1 <69813309+LukasGG1@users.noreply.github.com>
2020-08-17 16:45:48 -0500

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -8,7 +8,16 @@ namespace HelloWorld
     {
         public void Run()
         {
-
+            float health = 100.0f;
+            float healthRegen = 30;
+            string name = Console.ReadLine();
+            bool maxlevelreached = false;
+            int maxlevel = 50;
+            int level = 1;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.Write(name + " was healed by Gold Experience. Gained " + healthRegen + " health");
         }
     }
 }
